9a6cc5a5da617e4efe5bc5eed0b7edf64e8a745f
Gamer Mr <gitcuaac@gmail.com>
2024-10-22 22:13:21 +0500

    'complete'

--- javascript.js
+++ javascript.js
@@ -6,58 +6,102 @@ let ipadress = "http://127.0.0.1:3000/tasks"
 let data = [{
 }]
 
-
+init()
 async function get() {
     const res = await fetch(ipadress, {
         method: 'GET'
-    }).then
+    })
+    if(!res.ok){
+        console.log('ALERT!!!!')
+        return
+    }
     data = await res.json()
 }
-get()
-console.log(data)
 
 
-data.forEach((item) => {
-    if (item.id > counter) {
-        counter = item.id
+async function createTask(data) {
+    const res = await fetch(ipadress, {
+        method: 'POST',
+        body: JSON.stringify({
+            text: data.text,
+            isDone: data.isDone
+        })
+    })
+    if(!res.ok){
+        console.log('ALERT!!!!')
+        return
     }
-})
-if (counter > 1) {
-    counter++
+    return await res
+}
+
+async function  deleteTask(id) {
+    const res = await fetch(ipadress, {
+        method: "DELETE",
+        body: JSON.stringify({
+                id: id
+         })
+        
+    })
+    if(!res.ok){
+        console.log('ALERT!!!!')
+        return
+    }
+    return await res
+}
+async function  changeStatus(id) {
+    const res = await fetch(ipadress, {
+        method: "PATCH",
+        body: JSON.stringify({
+                id: id
+         })
+    })
+    if(!res.ok){
+        console.log('ALERT!!!!')
+        return
+    }
+    return await res
+}
+
+
+
+async function init(){
+
+    data.forEach((item) => {
+        if (item.id > counter) {
+            counter = item.id
+        } 
+        if (counter > 1) {
+            counter++
+        }
+    })
+    await get()
+    render()
+    console.log(data)
 }
 const mainbutton = document.getElementById("mainbutton")
 const list = document.getElementById("mylist")
 const textbox = document.getElementById("textbox")
-
-mainbutton.addEventListener('click', () => {
-    const textvalue = textbox.value
-    data.push({
-        id: counter++,
-        text: textvalue,
+mainbutton.addEventListener('click', async () => {
+    
+    const item = await createTask({
+        text: textbox.value,
         isDone: false
     })
-    sohranenie(data)
+    await get()
     render()
     textbox.value = ''
 })
-
-
 function createnew(objectdata) {
     let div = document.createElement('div')
     let input = document.createElement("input")
     let label = document.createElement('label')
     let button = document.createElement('button')
     let icon = document.createElement('img')
-
-
     label.htmlFor = 'input' + ++inputcounter
     label.className = 'app__list__item__text'
-
     label.innerText = objectdata.text
     input.type = 'checkbox'
-    input.id = 'input' + inputcounter
     button.className = 'app__list__item__icon'
-    button.id = 'button' + ++buttoncounter
     icon.src = 'https://cdn-icons-png.flaticon.com/512/1483/1483063.png'
     icon.style.width = "100%";
     if (objectdata.isDone == false) {
@@ -67,36 +111,23 @@ function createnew(objectdata) {
         input.checked = true
         div.className = "app__list__item_done"
     }
-    div.id = 'button' + buttoncounter
     button.appendChild(icon)
     div.appendChild(input)
     div.appendChild(label)
     div.appendChild(button)
 
+    button.addEventListener('click', async() => {
+        await deleteTask(objectdata.id)
+        await get()
 
-    //document.getElementById("mylist").appendChild(div)
-    button.addEventListener('click', () => {
-        let objcounter = 0
-        for (let item of data) {
-            if (objectdata.id == item.id) {
-                data.splice(objcounter, 1)
-            }
-            objcounter++
-        }
-        sohranenie(data)
         render()
-        //list.removeChild(document.getElementById(div.id))
     })
+    
+    input.addEventListener('change', async() => {
+        await changeStatus(objectdata.id)
+        await get()
+        render()
 
-    input.addEventListener('change', () => {
-        if (div.className == 'app__list__item') {
-            div.className = 'app__list__item_done'
-            objectdata.isDone = true
-        }
-        else {
-            div.className = 'app__list__item'
-        }
-        sohranenie(data)
     })
     return div
 }
@@ -109,8 +140,4 @@ function render() {
     }
 }
 
-function sohranenie(objectofdata) {
-    let temp = JSON.stringify(objectofdata)
-    localStorage.setItem('main', temp)
-}
 render()
