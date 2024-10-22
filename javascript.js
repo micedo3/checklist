4e08dcfb62fe00ffdd83c91b5cfb58b50061386a
unknown <gitcuaac@gmail.com>
2024-10-22 19:41:04 +0500

    '23'

--- javascript.js
+++ javascript.js
@@ -1,28 +1,35 @@
 let counter = 0
-let inputcounter= 0
+let inputcounter = 0
 let divid = 0
 let buttoncounter = 0
-
-let data = [{   
+let ipadress = "http://127.0.0.1:3000/tasks"
+let data = [{
 }]
 
 
-data = JSON.parse(localStorage.getItem('main'))
+async function get() {
+    const res = await fetch(ipadress, {
+        method: 'GET'
+    }).then
+    data = await res.json()
+}
+get()
+console.log(data)
 
 
-data.forEach((item)=>{
-    if(item.id > counter){
+data.forEach((item) => {
+    if (item.id > counter) {
         counter = item.id
     }
 })
-if(counter>1){
+if (counter > 1) {
     counter++
 }
 const mainbutton = document.getElementById("mainbutton")
 const list = document.getElementById("mylist")
 const textbox = document.getElementById("textbox")
 
-mainbutton.addEventListener('click', ()=> {
+mainbutton.addEventListener('click', () => {
     const textvalue = textbox.value
     data.push({
         id: counter++,
@@ -35,28 +42,28 @@ mainbutton.addEventListener('click', ()=> {
 })
 
 
-function createnew(objectdata){
+function createnew(objectdata) {
     let div = document.createElement('div')
     let input = document.createElement("input")
     let label = document.createElement('label')
     let button = document.createElement('button')
     let icon = document.createElement('img')
-    
-    
+
+
     label.htmlFor = 'input' + ++inputcounter
     label.className = 'app__list__item__text'
 
-    label.innerText= objectdata.text
-    input.type='checkbox'
+    label.innerText = objectdata.text
+    input.type = 'checkbox'
     input.id = 'input' + inputcounter
-    button.className ='app__list__item__icon'
+    button.className = 'app__list__item__icon'
     button.id = 'button' + ++buttoncounter
     icon.src = 'https://cdn-icons-png.flaticon.com/512/1483/1483063.png'
     icon.style.width = "100%";
-    if(objectdata.isDone == false){
-    div.className = "app__list__item"
+    if (objectdata.isDone == false) {
+        div.className = "app__list__item"
     }
-    else{
+    else {
         input.checked = true
         div.className = "app__list__item_done"
     }
@@ -65,14 +72,14 @@ function createnew(objectdata){
     div.appendChild(input)
     div.appendChild(label)
     div.appendChild(button)
-    
+
 
     //document.getElementById("mylist").appendChild(div)
-    button.addEventListener('click', ()=> {
+    button.addEventListener('click', () => {
         let objcounter = 0
-        for(let item of data){
-            if(objectdata.id==item.id){
-                data.splice(objcounter,1)
+        for (let item of data) {
+            if (objectdata.id == item.id) {
+                data.splice(objcounter, 1)
             }
             objcounter++
         }
@@ -81,28 +88,28 @@ function createnew(objectdata){
         //list.removeChild(document.getElementById(div.id))
     })
 
-    input.addEventListener('change', ()=> {
-        if (div.className == 'app__list__item'){
+    input.addEventListener('change', () => {
+        if (div.className == 'app__list__item') {
             div.className = 'app__list__item_done'
-            objectdata.isDone =true
+            objectdata.isDone = true
         }
-        else{
-            div.className ='app__list__item'
+        else {
+            div.className = 'app__list__item'
         }
         sohranenie(data)
     })
     return div
 }
 
-function render(){
+function render() {
     list.innerHTML = ''
-    for(let item of data){
+    for (let item of data) {
         const tempel = createnew(item)
         list.appendChild(tempel)
     }
 }
 
-function sohranenie(objectofdata){
+function sohranenie(objectofdata) {
     let temp = JSON.stringify(objectofdata)
     localStorage.setItem('main', temp)
 }
